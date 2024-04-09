bd6f590d58c373dc77ca5c3873fe91f1a9235035
burt2902 <burt2902@gmail.com>
2024-04-09 16:22:30 +0200

    scoreBoard class implementation
    Added implementation for startGame(), finishGame() and updateScore() methods

--- src/js/scoreboard.js
+++ src/js/scoreboard.js
@@ -1,22 +1,45 @@
 class ScoreBoard {
 
-    constructor() {
+    #gameInProgress = new Boolean();
+    #homeTeam = new String();
+    #awayTeam = new String();
+    #homeScore = new Number();
+    #awayScore = new Number();
+    #board = [];
 
+    constructor() {
+        this.#gameInProgress = false;
     }
 
     startGame(home, away) {
-        throw new Error('Not implemented!');
+        if(typeof(home) !== 'string' || typeof(away) !== 'string')
+            throw new Error('startGame: invalid parameters!');
+        if(this.#gameInProgress == true)
+            throw new Error('startGame: game already in progress!');
+        this.#homeTeam = home;
+        this.#awayTeam = away;
+        this.#homeScore = 0;
+        this.#awayScore = 0;
+        this.#gameInProgress = true;
     }
 
     finishGame() {
-        throw new Error('Not implemented!');
+        if(this.#gameInProgress == false)
+            throw new Error('updateScore: game not started!');
+        this.#board.push( [this.#homeTeam, this.#homeScore, this.#awayTeam, this.#awayScore] );
     }
 
     updateScore(home, away) {
-        throw new Error('Not implemented!');
+        if(typeof(home) !== 'number' || typeof(away) !== 'number' || home < 0 || away < 0)
+            throw new Error('updateScore: invalid parameters!');
+        if(this.#gameInProgress == false)
+            throw new Error('updateScore: game not started!');
+        this.#homeScore = home;
+        this.#awayScore = away;
     }
 
     *getSummary() {
+        console.log(this.#board);
         throw new Error('Not implemented!');
     }
 }
